08af2e48cf812a0f4df24d645e55bff97f9d3c19
sg1313 <fpemvps2@gmail.com>
2023-02-04 00:21:21 +0900

    settings.json수정

--- new_project/src/pages/Board.js
+++ new_project/src/pages/Board.js
@@ -6,30 +6,15 @@ const Board = () => {
   const onReset = () => {
     setText("");
   };
-
-  const [names, setNames] = useState([
-    { id: 1, text: "사과" },
-    { id: 2, text: "포도" },
-    { id: 3, text: "없어" },
-  ]);
-  const [inputText, setInputText] = useState();
-  const [nextId, setNextId] = useState(4);
+  
   const onChange = (e) => {
-    setText(e.target.value);
+    // setText(e.target.value);
+    console.log(e.target.value);
   };
   const onClickButton = (e) => {
-    const nextNames = names.concat({
-      id: nextId,
-      text: inputText,
-    });
-    setNextId(nextId + 1);
-    setNames(nextNames);
-    setInputText("");
 
     setText(e.target.value);
   };
-
-  const nameList = names.map((name) => <li>{name}</li>);
 
   return (
     <div>
@@ -39,7 +24,7 @@ const Board = () => {
       <button onClick={onReset}>초기화</button>
       <button onClick={onClickButton}>등록하기 </button>
       <br />
-      <ul>{nameList}</ul>
+      <ul>{text}</ul>
     </div>
   );
 };
